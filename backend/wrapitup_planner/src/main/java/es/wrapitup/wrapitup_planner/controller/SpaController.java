7d1064b219c3957377243ae03a6e382cf394508e
arturox2500 <gutierrezarturox@gmail.com>
2025-11-09 21:23:24 +0100

    .

--- backend/wrapitup_planner/src/main/java/es/wrapitup/wrapitup_planner/controller/SpaController.java
+++ backend/wrapitup_planner/src/main/java/es/wrapitup/wrapitup_planner/controller/SpaController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
-    @RequestMapping("/{path:[^\\.]*}")
-    public String forwardRoot() {
+
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}" })
+    public String forward() {
         return "forward:/index.html";
     }
-
 }
