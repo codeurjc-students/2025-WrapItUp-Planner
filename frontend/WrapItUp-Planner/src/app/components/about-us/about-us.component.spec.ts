46dc93f2789fd5295afdf34cc340cbff49a584eb
arturox2500 <gutierrezarturox@gmail.com>
2025-11-19 18:14:55 +0100

    Added About us page with styles and fixes to other elements

---
+++ frontend/WrapItUp-Planner/src/app/components/about-us/about-us.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from '../header/header.component';
+import { AboutUsComponent } from './about-us.component';
+
+describe('AboutUsComponent', () => {
+  let component: AboutUsComponent;
+  let fixture: ComponentFixture<AboutUsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AboutUsComponent, HeaderComponent],
+      imports: [
+        RouterTestingModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatCardModule
+      ]
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(AboutUsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
